f7db9b91a88d4cd640ac1cc0695f72b6752d56a3
UtasiB <utasibalazs@turr.hu>
2025-06-04 17:12:40 +0200

    dolgok

---
+++ Saját/Vizilabda/Vizilabda/Form1.Designer.cs
@@ -0,0 +1,400 @@
+﻿namespace Vizilabda
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.numericUpDown5 = new System.Windows.Forms.NumericUpDown();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(60, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(253, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bajnokásg felvétel";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(446, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(223, 40);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Kapítány felvétel";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(129, 47);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(159, 26);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(77, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Év:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(129, 79);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(159, 26);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(36, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Helyszín:";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(129, 111);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(159, 26);
+            this.numericUpDown2.TabIndex = 6;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(30, 111);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Helyezés:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(30, 141);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(74, 20);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Kapitány:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(129, 141);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(159, 28);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(30, 175);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(71, 20);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Verseny:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(129, 175);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(159, 26);
+            this.textBox2.TabIndex = 10;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(447, 67);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(40, 20);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Név:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(493, 64);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(159, 26);
+            this.textBox3.TabIndex = 13;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(493, 98);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(159, 26);
+            this.numericUpDown3.TabIndex = 14;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(410, 100);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(77, 20);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Született:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(417, 132);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(70, 20);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "Meghalt:";
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(493, 130);
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(159, 26);
+            this.numericUpDown4.TabIndex = 16;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(129, 207);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 46);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Felvétel";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(511, 162);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 46);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "Felvétel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(453, 225);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(199, 124);
+            this.listBox1.TabIndex = 20;
+            // 
+            // numericUpDown5
+            // 
+            this.numericUpDown5.Location = new System.Drawing.Point(493, 355);
+            this.numericUpDown5.Maximum = new decimal(new int[] {
+            2025,
+            0,
+            0,
+            0});
+            this.numericUpDown5.Name = "numericUpDown5";
+            this.numericUpDown5.Size = new System.Drawing.Size(122, 26);
+            this.numericUpDown5.TabIndex = 21;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(511, 387);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(89, 31);
+            this.button3.TabIndex = 22;
+            this.button3.Text = "Keres";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(511, 424);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(89, 31);
+            this.button4.TabIndex = 23;
+            this.button4.Text = "Összes";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label11
+            // 
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label11.Location = new System.Drawing.Point(12, 263);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(409, 40);
+            this.label11.TabIndex = 24;
+            this.label11.Text = "Európa bajnokság győzelmek";
+            this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(12, 306);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(409, 144);
+            this.listBox2.TabIndex = 25;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(720, 477);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.numericUpDown5);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Vizilabda";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ Saját/Vizilabda/Vizilabda/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using AFA_szamolo;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Vizilabda
+{
+
+    public partial class Form1 : Form
+    {
+        dbClass db = new dbClass("localhost", "root", "", "13a_vizilabda");    
+
+        public Form1()
+        {
+            InitializeComponent();
+            ComboBoxFeltoltes();
+            ListBoxToltes();
+            EuropaBajnoksagos();
+        }
+
+        private void EuropaBajnoksagos()
+        {
+            //A feladat azt írta hogy a "bajnokság tábla minden adata szerepeljen" ezért úgy gondolotam hogy nem csinálom meg azt hogy a nevét írja ki
+            //Lécci ne nagyon bántson ez miatt 😖🙏
+            listBox2.Items.Clear();
+            listBox2.Items.Add("Id - Év - Helyszín - Helyezés - Kapitány ID - Verseny");
+            db.selectAll("bajnoksag");
+            while (db.results.Read()) 
+            {
+                if (db.results["verseny"].ToString() == "Európa-bajnokság" && db.results["helyezes"].ToString() == "1") {
+                    listBox2.Items.Add(db.results["id"].ToString() + " - " + db.results["ev"].ToString() + " - " + db.results["helyszin"].ToString() + " - " + db.results["helyezes"].ToString()+ " - " + db.results["kapitanyid"].ToString() + " - " + db.results["verseny"].ToString());
+                }
+            }
+        }
+
+        private void ListBoxToltes()
+        {
+            listBox1.Items.Clear();
+            db.selectAll("kapitany");
+            while (db.results.Read())
+            {
+                listBox1.Items.Add(db.results["neve"].ToString());
+            }
+        }
+
+        private void ComboBoxFeltoltes()
+        {
+            try
+            {
+                db.selectAll("kapitany");
+                while (db.results.Read()) 
+                {
+                    comboBox1.Items.Add(db.results["neve"].ToString());
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Adatbázis hiba!");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (numericUpDown1.Value == 0 || textBox1.Text == "" || numericUpDown2.Value == 0 || textBox2.Text == "" || comboBox1.Text == "") 
+            {
+                MessageBox.Show("Hiányzó adatok!");
+            }
+           else 
+            {
+                try
+                {
+                    string[] fields = { "ev", "helyszin", "helyezes", "kapitanyid", "verseny" };
+                    string[] values = { numericUpDown1.Value.ToString(), textBox1.Text, numericUpDown2.Value.ToString(), (comboBox1.SelectedIndex + 1).ToString(), textBox2.Text };
+
+                    db.insert("bajnoksag", fields, values);
+                    MessageBox.Show("Sikeres felvétel!");
+
+                    numericUpDown1.Value = 1;
+                    textBox1.Text = "";
+                    numericUpDown2.Value = 1;
+                    textBox2.Text = "";
+                    comboBox1.Text = "";
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Hatalmas hiba van insertnél!");
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (numericUpDown3.Value == 0 || textBox3.Text == "")
+            {
+                MessageBox.Show("Hiányzó adatok!");
+            }
+            else
+            {
+                if (numericUpDown4.Value != 0)
+                {
+                    if (numericUpDown3.Value > numericUpDown4.Value)
+                    {
+                        MessageBox.Show("Itt nem adta ki a matek!");
+                    }
+                }
+                else
+                {
+                    try
+                    {
+                        string[] fields = { "neve", "szuletett", "meghalt", };
+                        string[] values = { textBox3.Text, numericUpDown3.Value.ToString(), numericUpDown4.Value.ToString() };
+
+                        db.insert("kapitany", fields, values);
+                        MessageBox.Show("Sikeres felvétel!");
+                        ComboBoxFeltoltes();
+
+                        numericUpDown4.Value = 1;
+                        textBox3.Text = "";
+                        numericUpDown3.Value = 1;
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("Hatalmas hiba van insertnél!");
+                    }
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int szulEv = Convert.ToInt32(numericUpDown5.Value);
+
+            listBox1.Items.Clear();
+            db.selectAll("kapitany");
+            while (db.results.Read())
+            {
+                if (Convert.ToInt32(db.results["szuletett"]) == szulEv)
+                {
+                    listBox1.Items.Add(db.results["neve"]);
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ListBoxToltes();
+        }
+    }
+}
